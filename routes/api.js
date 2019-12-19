fd2009a381ddd3c63fabc1c0bc6e4dc0eff8df3f
Sean Johnson <sean.johnson.ca@gmail.com>
2019-12-18 16:16:44 -0800

    geoname correction

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const axios = require('axios');
 
 const router = express.Router();
 const knex = require('../db/knex');
 
 require('dotenv').config();
+
+const COUNTRY_TYPE = 1;
 
 router.get('/api/artist-locations', async (req, res) => {
   const artists = JSON.parse(req.query.artists);
@@ -13,27 +16,43 @@ router.get('/api/artist-locations', async (req, res) => {
   const artistLocations = [];
 
   const promises = artists.map(async (artist) => {
-    const results = await knex
-      .select('area.name')
+    const words = artist.release.split(' ').map((w) => w.toLowerCase());
+    const wordsWithoutArticles = words.filter((w) => !['the', 'a', 'an', 'of', 'from'].includes(w));
+    const match = wordsWithoutArticles.length ? `%${wordsWithoutArticles[0]}%` : `%${words[0]}%`;
+
+    const rows = await knex
+      .select('area.*')
       .from('musicbrainz.artist AS artist')
       .join('musicbrainz.area AS area', 'artist.area', 'area.id')
-      .leftJoin('musicbrainz.release AS release', 'artist.id', 'release.artist_credit')
-      .modify((qb) => {
-        if (artist.release) {
-          const words = artist.release.split(' ').map((w) => w.toLowerCase());
-          const wordsWithoutArticles = words.filter((w) => !['the', 'a', 'an', 'of', 'from'].contains(w));
-          const match = wordsWithoutArticles.length ? `%${wordsWithoutArticles[0]}%` : `%${words[0]}%`;
-
-          qb.and('release.name', 'ilike', match);
-        }
-      })
+      .join('musicbrainz.release AS release', (qb) => {
+        qb.on('artist.id', '=', 'release.artist_credit');
+        qb.andOnVal('release.name', 'ilike', match);
+      }, 'left')
       .where('artist.name', 'ilike', artist.name)
       .limit(1);
 
-    if (results.length) {
+    console.log(rows);
+
+    if (rows.length) {
+      const row = rows[0];
+
+      let country = '';
+
+      if (row.type === COUNTRY_TYPE) {
+        country = row.name;
+      } else {
+        await axios.get(`http://api.geonames.org/searchJSON?q=${row.name}&maxRows=1&username=${process.env.GEONAMES_USER}`)
+          .then((result) => {
+            if (result.data.geonames && result.data.geonames.length) {
+              country = result.data.geonames[0].countryName;
+            }
+          })
+          .catch((err) => console.log(err));
+      }
+
       artistLocations.push({
         artist: artist.name,
-        country: results[0].name,
+        country,
       });
     }
   });
